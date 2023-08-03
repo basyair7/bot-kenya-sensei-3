16b9440c2fe01c5012e95e3eeea2ea61b34e178c
basyair7 <basyair.fathul@gmail.com>
2023-08-03 22:58:13 +0700

    add remove.ts in music commands

--- src/commands/debug/music/remove.ts
+++ src/commands/debug/music/remove.ts
@@ -18,54 +18,78 @@ const meta = new SlashCommandBuilder()
     );
 
 export default command(meta, async ({ interaction, client }) => {
-    const numDelete: number = interaction.options.getInteger("queue_number") as number;
-    if(queue.length == 1) {
-        const msg: object = new EmbedBuilder()
-            .setDescription(
-                `:x: **Can't remove when only one song is playing, Use command stop**`
-            )
-            .setTimestamp()
-            .setColor("Blue");
-        return interaction.reply({
-            ephemeral: true,
-            embeds: [ msg ]
-        });
-    }
-    if(numDelete > queue.length) {
-        const msg: object = new EmbedBuilder()
-            .setDescription(
-                `:x: **The queue doesn't have that much songs**`
-            )
-            .setTimestamp()
-            .setColor("Blue");
-        return interaction.reply({
-            ephemeral: true,
-            embeds: [ msg ]
-        });
-    }
-    if(queue.length === 0) {
-        const msg: object = new EmbedBuilder()
-            .setDescription(
-                `:x: **There are no songs playing in this server**`
-            )
-            .setTimestamp()
-            .setColor("Blue");
-        return interaction.reply({
-            ephemeral: true,
-            embeds: [ msg ]
-        });
-    }
-    const title: string = await queue[numDelete-1]["name"];
-    queue.splice(numDelete - 1);
+    try {
+        // ambil data users (query, id users, id member, id guild dan id channel)
+        const users = client.guilds.cache.get(interaction.guild?.id!);
+        const member = users?.members.cache.get(interaction.user.id);
+        const channelId = member?.voice.channelId;
 
-    const msg: object = new EmbedBuilder()
-        .setDescription(
-            `**Removed ${title} from queue :white_check_mark: **`
-        )
-        .setTimestamp()
-        .setColor("Blue");
-    return interaction.reply({
-        ephemeral: true,
-        embeds: [ msg ]
-    });
+        // jika member melakukan query tapi tidak ada di voice channel
+        if (!channelId) {
+            const message = new EmbedBuilder()
+                .setDescription("Kamu harus join voice channel dulu nak! :negative_squared_cross_mark:")
+                .setColor("Red");
+            return interaction.reply({
+                ephemeral: true,
+                embeds: [ message ]
+            });
+        }
+
+        const numDelete: number = interaction.options.getInteger("queue_number") as number;
+        if(queue.length == 1) {
+            const msg: object = new EmbedBuilder()
+                .setDescription(
+                    `:x: **Can't remove when only one song is playing, Use command stop**`
+                )
+                .setTimestamp()
+                .setColor("Blue");
+            return interaction.reply({
+                ephemeral: true,
+                embeds: [ msg ]
+            });
+        }
+        if(numDelete > queue.length) {
+            const msg: object = new EmbedBuilder()
+                .setDescription(
+                    `:x: **The queue doesn't have that much songs**`
+                )
+                .setTimestamp()
+                .setColor("Blue");
+            return interaction.reply({
+                ephemeral: true,
+                embeds: [ msg ]
+            });
+        }
+        if(queue.length === 0) {
+            const msg: object = new EmbedBuilder()
+                .setDescription(
+                    `:x: **There are no songs playing in this server**`
+                )
+                .setTimestamp()
+                .setColor("Blue");
+            return interaction.reply({
+                ephemeral: true,
+                embeds: [ msg ]
+            });
+        }
+
+        const title: string = await queue[numDelete-1]["name"];
+        queue.splice(numDelete - 1);
+
+        const msg: object = new EmbedBuilder()
+            .setDescription(
+                `**Removed ${title} from queue :white_check_mark: **`
+            )
+            .setTimestamp()
+            .setColor("Blue");
+
+        return interaction.reply({
+            fetchReply: true,
+            embeds: [ msg ]
+        });
+
+    } catch(e) {
+        console.error(e);
+    }
+    
 });
